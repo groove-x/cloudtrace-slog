2010e107f44b642255e4651b77f2ee354a092c4e
Ato Araki <ato.araki@gmail.com>
2025-08-04 08:54:52 +0900

    update example

--- example/gcp/main.go
+++ example/gcp/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"log"
 	"log/slog"
 	"net/http"
+	"os"
 
 	cloudtrace "github.com/groove-x/cloudtrace-slog"
 )
 
 func main() {
+	log.Print("starting server...")
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
 	// Setup slog
 	slog.SetDefault(slog.New(cloudtrace.NewCloudLoggingHandler()))
 
@@ -24,5 +35,9 @@ func main() {
 	// Wrap your handler with trace context middleware
 	handler := cloudtrace.WithCloudTraceContextMiddleware(mux)
 
-	http.ListenAndServe(":8080", handler)
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, handler); err != nil {
+		log.Fatal(err)
+	}
 }
